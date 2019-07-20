c552120f978bb87bef9a635012388aa18b6396a8
= <lidonghao@ke.com>
2019-07-20 22:57:58 +0800

    🔧  加点修饰符

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 rm -rf /usr/share/nginx/html/dist
-echo "旧的静态资源已删除"
+echo "🍓🍓🍓旧的静态资源已删除🍓🍓🍓"
 git checkout master
 git pull
-echo "新的代码已拉取"
+echo "🍓🍓🍓新的代码已拉取🍓🍓🍓"
 npm run build
 cp -r ./dist /usr/share/nginx/html/
+echo "🍉🍉🍉构建完毕,新版本已发布🍉🍉🍉"
